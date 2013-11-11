gammy/nes_gamepad

commit 6eb7a47347a5cba547910024a382784b50bcbfc1

Some refactoring. Remove some crud.

uinput-nes/src/main.c
@@ -35,17 +35,14 @@ int main(int argc, char *argv[]) {
 		{NULL,         0,                 NULL,  0}
 	};
 
-	uint8_t    last[PADS_MAX];
-	uint8_t     buf[PADS_MAX];
-	int      pad_fd[PADS_MAX];
-
-	uint8_t req;
 	int i;
+
+	pad_t pad[PADS_MAX];
 
 	int passthrough  = 0;
 	int buttons_only = 0;
 	int numpads      = 1;
-	verbosity       = 0;
+	verbosity        = 0;
 
 	while((i = getopt_long(argc, argv, "p:v:nPVh", long_options, NULL)) != -1){
 		switch(i) {
@@ -72,10 +69,6 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	if(verbosity > 0) {
-		fprintf(stderr, "Verbosity: %d\n", verbosity);
-	}
-
 	if(numpads < 1 || numpads > PADS_MAX) {
 		fprintf(stderr, "Only 1-%d gamepads can be used\n", PADS_MAX);
 		return(EXIT_FAILURE);
@@ -87,14 +80,13 @@ int main(int argc, char *argv[]) {
 
 	for(i = 0; i < numpads; i++) {
 
-		pad_fd[i] = uinput_init(1 + i, buttons_only);
+		memset(pad, 0, sizeof(pad_t));
 
-		if(pad_fd[i] < 0)
-			return(pad_fd[i]);
+		pad[i].num = 1 + i;
+		pad[i].fd = uinput_init(pad[i].num, buttons_only);
 
-		if(verbosity > 1)
-			fprintf(stderr, "Gamepad %d fd = %d\n", 1 + i, pad_fd[i]);
-
+		if(pad[i].fd < 0)
+			return(pad[i].fd);
 
 	}
 
@@ -114,16 +106,18 @@ int main(int argc, char *argv[]) {
 
 		for(i = 0; i < numpads; i++) {
 
-			req = i;
+			pad_t *p = &pad[i];
 
 			ftdi_usb_purge_rx_buffer(ftdic);
 
-			bub_send(ftdic, &req, sizeof(req));
-			bub_fetch(ftdic, &buf[i], sizeof(req));
+			uint8_t req = i;
 
-			if(buf[i] != last[i] || passthrough) {
-				uinput_map(pad_fd[i], buf[i], buttons_only);
-				last[i] = buf[i];
+			bub_send(ftdic, &req, sizeof(req));
+			bub_fetch(ftdic, &p->state, sizeof(req));
+
+			if(p->state != p->last || passthrough) {
+				uinput_map(p, buttons_only);
+				p->last = p->state;
 			}
 
 
@@ -132,7 +126,7 @@ int main(int argc, char *argv[]) {
 
 	}
 
-	fprintf(stderr, "Caught signal %d\n", interrupt);
+	fprintf(stderr, "\nCaught signal %d\n", interrupt);
 
 	printf("Closing serial interface\n");
 
@@ -148,14 +142,8 @@ int main(int argc, char *argv[]) {
 
 	printf("Closing gamepad device%s\n", numpads == 1 ? "" : "s");
 
-	for(i = 0; i < numpads; i++) {
-
-		if(ioctl(pad_fd[i], UI_DEV_DESTROY) < 0) {
-			perror("ioctl");
-		}
-
-		close(pad_fd[i]);
-	}
+	for(i = 0; i < numpads; i++)
+		uinput_deinit(&pad[i]);
 
 	return(EXIT_SUCCESS);
 }

uinput-nes/src/uinput.c
@@ -28,6 +28,9 @@ int uinput_init(int device_number, int buttons_only) {
 		return(fd);
 	}
 
+	if(verbosity > 1)
+		fprintf(stderr, "Gamepad %d (fd %d)\n", device_number, fd);
+
 	// Add Key(buttons) type
 	r = ioctl(fd, UI_SET_EVBIT, EV_KEY);
 	if(r < 0) { 
@@ -104,10 +107,19 @@ int uinput_init(int device_number, int buttons_only) {
 		return(r);
 	}
 
-	if(verbosity > 1)
-		fprintf(stderr, "Device %d fd = %d\n", device_number, fd);
-
 	return(fd);
+
+}
+
+void uinput_deinit(pad_t *pad) {
+
+	if(verbosity > 1)
+		fprintf(stderr, "Close %d (fd %d)\n", pad->num, pad->fd);
+
+	if(ioctl(pad->fd, UI_DEV_DESTROY) < 0)
+		perror("ioctl");
+
+	close(pad->fd);
 
 }
 
@@ -142,16 +154,20 @@ void printbits(uint8_t b) {
 	return;
 }
 
-void uinput_map(int fd, uint8_t state, int buttons_only) {
+void uinput_map(pad_t *pad, int buttons_only) {
 
 	static unsigned long count = 0;
 
+	uint8_t state = pad->state;
+	int        fd = pad->fd;
+
 	if(verbosity > 0) {
-		printf("%8ld: Pad fd %d: ", count, fd);
-		printf("%3d (%2x): ", state, state);
+
+		printf("%8ld Pad %d: ", count, pad->num);
+		printf("%3d (%2x) [", state, state);
 		printbits(state);
 
-		printf(" Right(%d)",    IS_RIGHT(state));
+		printf("] Right(%d)",   IS_RIGHT(state));
 		printf(" Left(%d)",      IS_LEFT(state));
 		printf(" Down(%d)",      IS_DOWN(state));
 		printf(" Up(%d)",          IS_UP(state));
@@ -161,21 +177,6 @@ void uinput_map(int fd, uint8_t state, int buttons_only) {
 		printf(" A(%d)",            IS_A(state));
 		puts("");
 
-		count++;
-	} else if(verbosity > 1) {
-		printf("%8ld: Pad fd %d: ", count, fd);
-		printf("%3d (%2x): ", state, state);
-		printbits(state);
-
-		printf(" Right");  printbits( IS_RIGHT(state));
-		printf(" Left");   printbits(  IS_LEFT(state));
-		printf(" Down");   printbits(  IS_DOWN(state));
-		printf(" Up");     printbits(    IS_UP(state));
-		printf(" Start");  printbits( IS_START(state));
-		printf(" Select"); printbits(IS_SELECT(state));
-		printf(" B");      printbits(     IS_B(state));
-		printf(" A");      printbits(     IS_A(state));
-		puts("");
 		count++;
 	}
 

uinput-nes/src/uinput.h
@@ -20,8 +20,16 @@
 #define AXIS_MIN        -32767
 #define AXIS_MAX         32767
 
-int uinput_init(int device_number, int noaxis);
-int uinput_send(int fd, uint16_t type, uint16_t code, int32_t val);
-void uinput_map(int fd, uint8_t state, int noaxis);
+typedef struct {
+	int fd;
+	int num;
+	uint8_t state;
+	uint8_t last;
+} pad_t;
+
+int  uinput_init(int device_number, int buttons_only);
+void uinput_deinit(pad_t *pad);
+int  uinput_send(int fd, uint16_t type, uint16_t code, int32_t val);
+void uinput_map(pad_t *pad, int buttons_only);
 
 #endif

commit 13780a6888a39951c4f46483d34e25b189a1bbaf

Minor message changes. Fix serious pad init bug introduced in 6eb7a47347a5cba547910024a382784b50bcbfc1

uinput-nes/src/main.c
@@ -84,7 +84,7 @@ int main(int argc, char *argv[]) {
 
 	for(i = 0; i < numpads; i++) {
 
-		memset(pad, 0, sizeof(pad_t));
+		memset(&pad[i], 0, sizeof(pad_t));
 
 		pad[i].num = 1 + i;
 		pad[i].fd = uinput_init(pad[i].num, buttons_only);

uinput-nes/src/uinput.c
@@ -18,6 +18,11 @@ int uinput_init(int device_number, int buttons_only) {
 	else
 		numbuttons = 4;
 
+	if(device_number == 1) { // XXX not so elegant, but does the job
+		for(i = 0; i < numbuttons; i++)
+			printf("Button %d: %s\n", 1 + i, button_name[i]);
+	}
+
 	if(verbosity > 1)
 		fprintf(stderr, "uinput_init(%d)\n", device_number);
 
@@ -47,13 +52,12 @@ int uinput_init(int device_number, int buttons_only) {
 
 	// Add the buttons
 	for(i = 0; i < numbuttons; i++) {
-		if(verbosity > 1)
-			fprintf(stderr, "uinput: Adding button %d type %04x: %s\n", 
+		if(verbosity > 1) {
+			fprintf(stderr, "Adding key %d type %03x (%s)\n", 
 				1 + i, 
 				button[i],
 				button_name[i]);
-		else
-			printf("Button %d: %s\n", 1 + i, button_name[i]);
+		}
 
 		r = ioctl(fd, UI_SET_KEYBIT, button[i]);
 		if(r < 0) {
@@ -64,11 +68,10 @@ int uinput_init(int device_number, int buttons_only) {
 
 	if(! buttons_only) {
 		// Add the axes
-		for(i = 0; i < sizeof(axis) / sizeof(int); i++) {
+		for(i = 0; i < 2; i++) {
 
 			if(verbosity > 1) {
-				fprintf(stderr, 
-					"uinput: Adding axis %d, type %04x\n", 
+				fprintf(stderr, "Adding axis %d, type %03x\n", 
 					i, axis[i]);
 			}
 
@@ -115,7 +118,7 @@ int uinput_init(int device_number, int buttons_only) {
 void uinput_deinit(pad_t *pad) {
 
 	if(verbosity > 1)
-		fprintf(stderr, "Close %d (fd %d)\n", pad->num, pad->fd);
+		fprintf(stderr, "Closing pad %d (fd %d)\n", pad->num, pad->fd);
 
 	if(ioctl(pad->fd, UI_DEV_DESTROY) < 0)
 		perror("ioctl");